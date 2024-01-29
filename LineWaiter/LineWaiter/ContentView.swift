d27bb41c71cfa7994271ebc7396de1da15dde7cd
Nathaniel Thomas <55164518+Nathaniel-github@users.noreply.github.com>
2024-01-29 11:24:24 -0800

    Xcode skeleton

---
+++ LineWaiter/LineWaiter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LineWaiter
+//
+//  Created by Nathaniel Thomas on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LineWaiter/LineWaiter/LineWaiterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineWaiterApp.swift
+//  LineWaiter
+//
+//  Created by Nathaniel Thomas on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LineWaiterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
