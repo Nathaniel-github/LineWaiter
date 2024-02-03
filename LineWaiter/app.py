bff6e88a45f26e22159d6879d838fd54b4598d92
Athena Mo <50725534+Fishier1224@users.noreply.github.com>
2024-02-03 13:44:57 -0800

    Update app.py

--- LineWaiter/app.py
+++ LineWaiter/app.py
@@ -9,6 +9,15 @@ def main():
 #def hello_world():  # put application's code here
     #return 'Hello World!'
 
+@app.route('/ask/', methods=['POST', 'GET'])
+def ask():
+    if request.method == 'GET':
+        return render_template('userInfo.html')
+    else:
+        try:
+            return render_template('userInfo.html', name=request.form['name'], request=request.form['request'])
+        except:
+            return render_template('userInfo.html')
 
 if __name__ == '__main__':
     app.run()
