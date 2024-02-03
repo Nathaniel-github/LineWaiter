b099b3eba836c7b751d3b1dd2a829e0eb0c847cb
syu5297 <selenayu@g.ucla.edu>
2024-02-03 15:09:40 -0800

    listings in mainlistings

--- LineWaiter/app.py
+++ LineWaiter/app.py
@@ -35,6 +35,21 @@ def my_listings():
             return render_template('yourListings.html')
     return render_template('yourListings.html')
 
+class Listing:
+    def __init__(self, title, additional_info):
+        self.title = title
+        self.additional_info = additional_info
+
+@app.route('/')
+def listings():
+    # Create a list of Listing instances
+    listings = [
+        Listing("Listing Title 1", "Additional information about Listing 1."),
+        Listing("Listing Title 2", "Additional information about Listing 2."),
+        Listing("List 3", "Additional information about List 3.")
+    ]
+
+    return render_template('mainListings.html', listings=listings)
 
 if __name__ == '__main__':
     app.run()
