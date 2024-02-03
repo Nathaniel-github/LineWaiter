02e7baedf7f39c31a7eeaf6cc12ef80850e69efc
Athena Mo <50725534+Fishier1224@users.noreply.github.com>
2024-02-03 13:38:46 -0800

    Update app.py

--- LineWaiter/app.py
+++ LineWaiter/app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    return render_template('main.html')
+
+#def hello_world():  # put application's code here
+    #return 'Hello World!'
 
 
 if __name__ == '__main__':
