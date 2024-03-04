fe80dce7ceab140c3707a29ee093f14091e66fd5
Athena Mo <Athena_1224@outlook.com>
2024-03-03 18:53:19 -0800

    deleted length

--- LineWaiter/app.py
+++ LineWaiter/app.py
@@ -103,7 +103,7 @@ def my_listings():
         try:
             database.add_listing(Listing(**request.form))
             return render_template('myListings.html', where=request.form['where'], when=request.form['when'],
-                                  length=request.form['length'], price=request.form['price'], description=request.form['description'],
+                                  price=request.form['price'], description=request.form['description'],
                                    name=request.form['name'], duration=request.form['duration'])
         except:
             return render_template('myListings.html')
