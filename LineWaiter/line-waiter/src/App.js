fc1c0460e3abec8ea525d95ed9e8c67b032e7be6
pjoshi23 <star.flight431@gmail.com>
2024-03-04 18:46:00 -0800

    updated routing information for homepage and create_listing

--- LineWaiter/line-waiter/src/App.js
+++ LineWaiter/line-waiter/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/signup";
 import Login from "./components/login";
+import Create_Listing from "./components/create_listing/create_listing";
+import HomePage from "./components/Main";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -11,10 +13,8 @@ function App() {
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
-			<Route path="/all-listings" exact element={<All-Listings />} />
-			<Route path="/my-listings" exact element={<My-Listings />} />
-			<Route path="/create-listing" exact element={<Create-Listing />} />
-			<Route path="/profile-info" exact element={<Profile-Info />} />
+			<Route path="/all-listings" exact element={<HomePage />} />
+			<Route path="/create-listing" exact element={<Create_Listing />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
 	);
