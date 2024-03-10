ffd03671dddfc24302220154db835d0e99f4c55a
pjoshi23 <star.flight431@gmail.com>
2024-03-09 19:06:15 -0800

    fetch method for signup page

--- LineWaiter/line-waiter/src/pages/signup.jsx
+++ LineWaiter/line-waiter/src/pages/signup.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import './login.module.css'
 import { FaUser, FaLock } from 'react-icons/fa'
 import { sha3_256 } from 'js-sha3'
+import styles from "./login.module.css";
 
 const SignupForm = ( ) => {
 
@@ -28,30 +29,43 @@ const SignupForm = ( ) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const hashedPassword = sha3_256(password);
+    const hashedPassword = sha3_256(password.toString());
     let data = {
-      username: username,
+      username: username[""],
       password: hashedPassword,
     };
+    console.log("reached");
     // Handle form submission logic here
     console.log(data);
+
+    fetch('/createAnAccount/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => {
+        console.log(res);
+    })
+
   };
 
     return (
-        <div className="wrapper">
-            <form action="">
-                <h1>Sign Up</h1>
-                <div className="input-box">
-                    <input type="text" onChange={handleChangeUsername} placeholder="Username" required />
-                    <FaUser className="icon"/>
-                </div>
-                <div className="input-box">
-                    <input type="password" onChange={handleChangePassword} placeholder="Password" required />
-                    <FaLock className="icon"/>
-                </div>
-
-                <button type="submit" onClick={handleSubmit}>Sign Up</button>
-            </form>
+        <div className={styles.login}> {/* Use className from CSS module */}
+            <div className={styles.wrapper}> {/* Use className from CSS module */}
+                <form action="">
+                    <h1>Sign Up</h1>
+                    <div className={styles['input-box']}> {/* Use className from CSS module */}
+                        <input type="text" onChange={handleChangeUsername} placeholder="username" required/>
+                        <FaUser className={styles.icon}/>
+                    </div>
+                    <div className={styles['input-box']}> {/* Use className from CSS module */}
+                        <input type="password" onChange={handleChangePassword} placeholder="Password" required/>
+                        <FaLock className={styles.icon}/>
+                    </div>
+                    <button type="submit" onClick={handleSubmit}>Sign Up</button>
+                </form>
+            </div>
         </div>
     )
 }
