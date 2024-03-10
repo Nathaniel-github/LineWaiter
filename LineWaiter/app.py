acbbf3f87cd2f4b816238f5f67d9c0f12aa201c9
Nathaniel Thomas <55164518+Nathaniel-github@users.noreply.github.com>
2024-03-09 19:54:50 -0800

    Clean up app.py and add a return for route without one

--- LineWaiter/app.py
+++ LineWaiter/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 
 import os
 from dotenv import load_dotenv
-import re
 
 from db import Listing, Database, User
 
@@ -12,27 +11,27 @@ load_dotenv(".env")
 DB_PSWD = os.getenv("DB_PSWD")
 database = Database(DB_PSWD)
 
-@app.route('/login/', methods=['POST', 'GET'])
+@app.route('/login/', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        try:
-            username = request.form['username']
-            password = request.form['password']
-            user = database.get_user(username)
-            if user.password == password:
-                return {"auth": "success"}
-            else:
-                return {"auth": "failure"}
-        except:
+    try:
+        username = request.form['username']
+        password = request.form['password']
+        user = database.get_user(username)
+        if user.password == password:
+            return {"auth": "success"}
+        else:
             return {"auth": "failure"}
+    except:
+        return {"auth": "failure"}
 
 
 @app.route('/createAnAccount/', methods=['POST'])
 def create_an_account():
     database.add_user(User(**request.form))
+    return {"status": "success"}
 
 
-@app.route('/allListings')
+@app.route('/allListings', methods=['GET'])
 def get_listings():
     return database.get_all_listings()
 
