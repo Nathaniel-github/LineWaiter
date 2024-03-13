f17133f88344cdfb93ba665e775bf0b6be60ae89
Athena Mo <Athena_1224@outlook.com>
2024-03-13 00:09:26 -0700

    Clarified listing display

--- LineWaiter/line-waiter/src/components/myListingsListing.jsx
+++ LineWaiter/line-waiter/src/components/myListingsListing.jsx
@@ -28,7 +28,7 @@ const MyListingsListing = ({ _id, title, location, time, duration, price, descri
                         <section className="listing-details">
                             <h2 className="listing-subtext">location: {location}</h2>
                             <h2 className="listing-subtext">time: {time}</h2>
-                            <h2 className="listing-subtext">duration: {duration}</h2>
+                            <h2 className="listing-subtext">duration in minutes: {duration}</h2>
                             <h2 className="listing-subtext">price: {price}</h2>
                             <h2 className="listing-subtext">description: {description}</h2>
                         </section>
